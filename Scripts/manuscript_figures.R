d442b363c2efcad3168bb0cfff0e34bcbfdfde08
Rene Clark <31482962+rclark848@users.noreply.github.com>
2023-06-20 11:36:10 -0400

    added package versions

--- Scripts/manuscript_figures.R
+++ Scripts/manuscript_figures.R
@@ -14,9 +14,9 @@ getwd() #check working directory
 library(here) #v.1.0.1
 library(data.table) #v.1.14.8
 library(tidyverse) #v.2.0.0
-library(ggpattern) #
+library(ggpattern) #v.1.0.1
 library(gridExtra) #v.2.3
-library(cowplot)
+library(cowplot) #v.1.1.1
 library(ggthemes) #v.4.2.4
 
 #read in data
